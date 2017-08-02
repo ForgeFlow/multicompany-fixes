08f3a60bdbad7f50f7c180af32e8322176f063b2
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-08-02 16:42:45 +0530

    [IMP] fiscal position comodel constraint

--- mcfix_purchase/models/__init__.py
+++ mcfix_purchase/models/__init__.py
@@ -2,3 +2,4 @@
 from . import purchase
 from . import account_invoice
 from . import account_tax
+from . import account_fiscal_position
---
+++ mcfix_purchase/models/account_fiscal_position.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Creu Blanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class AccountFiscalPosition(models.Model):
+    _inherit = 'account.fiscal.position'
+
+    @api.multi
+    @api.constrains('company_id')
+    def _check_purchase_order_company(self):
+        for rec in self:
+            if rec.company_id:
+                orders = self.env['purchase.order'].search(
+                    [('fiscal_position_id', '=', rec.id),
+                     ('company_id', '!=', rec.company_id.id)], limit=1)
+                if orders:
+                    raise ValidationError(_('Purchase orders already exist '
+                                            'referencing this fiscal position '
+                                            'in other company : %s.') %
+                                          orders.company_id.name)
