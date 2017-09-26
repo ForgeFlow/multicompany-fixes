f4ab83b4abde7240c3eb485cb4f4201e2062d0dd
mreficent <miquel.raich@eficent.com>
2017-09-26 17:51:05 +0200

    [ADD] mcfix_account_move_line_reconcile

--- mcfix_account/models/account_move.py
+++ mcfix_account/models/account_move.py
@@ -331,3 +331,22 @@ class AccountMoveLine(models.Model):
                 raise ValidationError(_('The Company in the Move Line and in '
                                         'Partner must be the same.'))
         return True
+
+
+class AccountMoveLineReconcile(models.Model):
+    _inherit = 'account.move.line.reconcile'
+
+    @api.multi
+    @api.depends('company_id')
+    def name_get(self):
+        res = []
+        names = super(AccountMoveLineReconcile, self).name_get()
+        multicompany_group = self.env.ref('base.group_multi_company')
+        if multicompany_group not in self.env.user.groups_id:
+            return names
+        for name in names:
+            rec = self.browse(name[0])
+            name = '%s [%s]' % (name[1], name.company_id.name) if \
+                name.company_id else name[1]
+            res += [(rec.id, name)]
+        return res
