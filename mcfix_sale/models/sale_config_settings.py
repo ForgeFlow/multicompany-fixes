473e0ddddde84bd7b806b2b8bd182bc523652ee3
mreficent <miquel.raich@eficent.com>
2017-09-29 12:36:07 +0200

    [ADD][mcfix_sale] sale_config_settings

---
+++ mcfix_sale/models/sale_config_settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models
+
+
+class SaleConfiguration(models.TransientModel):
+    _inherit = 'sale.configuration'
+
+    @api.multi
+    @api.depends('company_id')
+    def name_get(self):
+        res = []
+        names = super(SaleConfiguration, self).name_get()
+        multicompany_group = self.env.ref('base.group_multi_company')
+        if multicompany_group not in self.env.user.groups_id:
+            return names
+        for name in names:
+            rec = self.browse(name[0])
+            name = '%s [%s]' % (name[1], name.company_id.name) if \
+                name.company_id else name[1]
+            res += [(rec.id, name)]
+        return res
