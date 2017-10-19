3ae111c1b2dfb4c3430a9f3843c778359f3253fa
mreficent <miquel.raich@eficent.com>
2017-10-19 19:26:41 +0200

    [FIX] mcfix_base

--- mcfix_base/models/res_partner.py
+++ mcfix_base/models/res_partner.py
@@ -32,22 +32,3 @@ class ResPartner(models.Model):
         compute='_compute_current_company_id',
         store=False
     )
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    @api.multi
-    @api.depends('company_id')
-    def name_get(self):
-        res = []
-        names = super(ResUsers, self).name_get()
-        multicompany_group = self.env.ref('base.group_multi_company')
-        if multicompany_group not in self.env.user.groups_id:
-            return names
-        for name in names:
-            rec = self.browse(name[0])
-            name = '%s [%s]' % (name[1], rec.company_id.name) if \
-                rec.company_id else name[1]
-            res += [(rec.id, name)]
-        return res
