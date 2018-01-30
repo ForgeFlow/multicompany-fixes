dd907b49afe9210762c4639a5949f34b9c9b447e
Enric Tobella <etobella@creublanca.es>
2018-01-30 16:24:58 +0100

    [ADD] multicompany_property_stock_account

---
+++ multicompany_property_stock_account/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Multi Company Stock Account',
+    'version': '11.0.1.0.0',
+    'summary': 'Stock Account Company Properties',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'sequence': 30,
+    'license': 'LGPL-3',
+    'website': 'http://www.eficent.com',
+    'depends': ['multicompany_property_account',
+                'multicompany_property_stock'],
+    'data': [
+        'views/product.xml',
+        'views/product_category.xml',
+        'views/res_company_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
