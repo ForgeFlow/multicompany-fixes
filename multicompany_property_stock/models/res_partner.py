533f1eacd4655b1e4b8790f0d7ec907e8539f472
Enric Tobella <etobella@creublanca.es>
2018-01-30 16:24:50 +0100

    [ADD] multicompany_property_stock

---
+++ multicompany_property_stock/models/res_partner.py
@@ -0,0 +1,48 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models, fields, api
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    property_stock_customer = fields.Many2one(readonly=True)
+    property_stock_supplier = fields.Many2one(readonly=True)
+
+
+class PartnerProperty(models.TransientModel):
+    _inherit = 'res.partner.property'
+
+    property_stock_customer = fields.Many2one(
+        'stock.location', string="Customer Location",
+        compute='_compute_property_fields',
+        readonly=False,
+        help="This stock location will be used, instead of "
+             "the default one, as the destination location for "
+             "goods you send to this partner")
+    property_stock_supplier = fields.Many2one(
+        'stock.location', string="Vendor Location",
+        compute='_compute_property_fields',
+        readonly=False,
+        help="This stock location will be used, instead of "
+             "the default one, as the source location for goods "
+             "you receive from the current partner")
+
+    @api.multi
+    def get_property_fields(self, object, properties):
+        super(PartnerProperty, self).get_property_fields(
+            object, properties)
+        for rec in self:
+            rec.property_stock_customer = rec.get_property_value(
+                'property_stock_customer', object, properties)
+            rec.property_stock_supplier = rec.get_property_value(
+                'property_stock_supplier', object, properties)
+
+    @api.multi
+    def get_property_fields_list(self):
+        res = super(PartnerProperty, self).get_property_fields_list()
+        res.append('property_stock_customer')
+        res.append('property_stock_supplier')
+        return res
