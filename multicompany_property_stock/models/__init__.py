bc9f5fb591fd799a1f69d48fd84fd812f2036d18
Enric Tobella <etobella@creublanca.es>
2018-01-30 16:24:50 +0100

    [ADD] multicompany_property_stock

---
+++ multicompany_property_stock/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import product
+from . import res_partner
+from . import stock_warehouse
