5332fb075a9620bacda2226b135b5a8de889b1b1
Enric Tobella <etobella@creublanca.es>
2018-01-30 16:25:08 +0100

    multicompany_property_purchase

---
+++ multicompany_property_purchase/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Multi Company Purchase',
+    'version': '11.0.1.0.0',
+    'summary': 'Purchase Company Properties',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'sequence': 30,
+    'license': 'LGPL-3',
+    'website': 'http://www.eficent.com',
+    'depends': ['purchase', 'multicompany_property_stock_account'],
+    'data': [
+        'views/partner.xml',
+        'views/product.xml',
+        'views/product_category.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
