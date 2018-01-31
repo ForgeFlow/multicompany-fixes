3fa57506753548d2f8e46eec5a6e0cd6dea76df3
Enric Tobella <etobella@creublanca.es>
2018-01-31 09:38:06 +0100

    multicompany_property_account: Configure accounting on company view.

--- multicompany_property_account/models/__init__.py
+++ multicompany_property_account/models/__init__.py
@@ -2,6 +2,8 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
+from . import chart_template
+from . import res_company
 from . import res_partner
 from . import product
 from . import product_category
