9fa1984690e80479019d2635382f153d96e6c86d
mreficent <miquel.raich@eficent.com>
2018-02-09 18:17:36 +0100

    [ADD] mcfix_account

---
+++ mcfix_payment/models/account_journal.py
@@ -0,0 +1,23 @@
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    @api.constrains('company_id')
+    def _check_company_id_out_model(self):
+        super(AccountJournal, self)._check_company_id_out_model()
+        if not self.env.context.get('bypass_company_validation', False):
+            for rec in self:
+                if not rec.company_id:
+                    continue
+                field = self.env['payment.acquirer'].search(
+                    [('journal_id', '=', rec.id),
+                     ('company_id', '!=', False),
+                     ('company_id', '!=', rec.company_id.id)], limit=1)
+                if field:
+                    raise ValidationError(
+                        _('You cannot change the company, as this '
+                          'Account Journal is assigned to Payment Acquirer '
+                          '(%s).' % field.name_get()[0][1]))
