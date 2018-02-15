b795b7df358c2e4c7bde94b79d06c47bccccafa8
Richard deMeester <richard@willowit.com.au>
2018-02-15 14:53:36 +1100

    [10.0] [FIX] account.payment company_id is related
    Since company_id is related from journal_id, it makes no sense to use
    the company_id to restrict the journal!

--- mcfix_account/models/account_payment.py
+++ mcfix_account/models/account_payment.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, api, _
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
@@ -65,6 +65,15 @@ class AccountAbstractPayment(models.AbstractModel):
     _inherit = 'account.abstract.payment'
 
     @api.multi
+    def _get_default_company(self):
+        if self.env.context.get('default_invoice_ids'):
+            inv = self.env['account.invoice'].browse(self.env.context['default_invoice_ids'][0][1])
+            return inv.company_id.id
+        return self.env.user.company_id.id
+
+    create_company_id = fields.Many2one('res.company', string='Company', default=_get_default_company)
+
+    @api.multi
     @api.depends('company_id')
     def name_get(self):
         res = []
@@ -79,10 +88,12 @@ class AccountAbstractPayment(models.AbstractModel):
             res += [(rec.id, name)]
         return res
 
-    @api.onchange('company_id')
-    def onchange_company_id(self):
-        self.journal_id = False
+#     @api.onchange('create_company_id')
+#     def onchange_company_id(self):
+#         if self.create_company_id and self.journal_id.company_id != self.create_company_id:
+#             self.journal_id = False
 
+    # Company is related from journal_id cannot be different!!!!!
     @api.multi
     @api.constrains('journal_id', 'company_id')
     def _check_company_journal_id(self):
@@ -94,6 +105,27 @@ class AccountAbstractPayment(models.AbstractModel):
                     _('The Company in the Account Payment and in '
                       'Payment Journal must be the same.'))
         return True
+
+
+class AccountRegisterPayment(models.TransientModel):
+    _inherit = 'account.register.payments'
+
+    @api.model
+    def default_get(self, fields):
+        rec = super(AccountRegisterPayment, self).default_get(fields)
+        context = dict(self._context or {})
+        active_model = context.get('active_model')
+        active_ids = context.get('active_ids')
+        invoices = self.env[active_model].browse(active_ids)
+        if invoices:
+            rec['create_company_id'] = invoices[0].company_id.id
+        return rec
+
+    @api.onchange('create_company_id')
+    def onchange_company_id(self):
+        if self.create_company_id and \
+                self.journal_id.company_id != self.create_company_id:
+            self.journal_id = False
 
 
 class AccountPayment(models.Model):
@@ -131,17 +163,26 @@ class AccountPayment(models.Model):
                         force_company=rec.company_id.id).\
                         property_account_payable_id.id
 
-    @api.onchange('payment_type', 'company_id')
+    @api.onchange('create_company_id')
+    def onchange_company_id(self):
+        if self.create_company_id and \
+                self.journal_id.company_id != self.create_company_id:
+            self.journal_id = False
+        if self.create_company_id:
+            return self._onchange_payment_type()
+
+    @api.onchange('payment_type')
     def _onchange_payment_type(self):
         res = super(AccountPayment, self)._onchange_payment_type()
         res['domain']['journal_id'].append(('company_id', '=',
-                                            self.company_id.id))
+                                            self.create_company_id.id))
         return res
 
-    @api.model
-    def create(self, vals):
-        if 'company_id' not in vals:
-            journal_id = vals.get('journal_id')
-            journal = self.env['account.journal'].browse(journal_id)
-            vals['company_id'] = journal.company_id.id
-        return super(AccountPayment, self).create(vals)
+    # is related!!!
+#     @api.model
+#     def create(self, vals):
+#         if 'company_id' not in vals:
+#             journal_id = vals.get('journal_id')
+#             journal = self.env['account.journal'].browse(journal_id)
+#             vals['company_id'] = journal.company_id.id
+#         return super(AccountPayment, self).create(vals)
