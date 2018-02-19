cde022f2016ba50ef45e63bcbe763629131a158e
mreficent <miquel.raich@eficent.com>
2018-02-19 18:59:25 +0100

    [FIX] mcfix_account: reports and tests

---
+++ mcfix_web/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Multi Company Fix Web',
+    'version': '11.0.1.0.0',
+    'summary': 'Web fixes',
+    'author': 'Eficent, Odoo Community Association (OCA)',
+    'website': 'http://www.eficent.com',
+    'license': 'LGPL-3',
+    'depends': ['web', 'mcfix_base'],
+    'data': [
+        'views/report_templates.xml',
+    ],
+    'sequence': 30,
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
