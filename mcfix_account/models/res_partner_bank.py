e9af7737a2c1fbdce9dfe71e2cec1d0334195e2c
mreficent <miquel.raich@eficent.com>
2018-02-21 13:05:57 +0100

    [ADD] mcfix_account_asset (& FIX mcfix_account)

--- mcfix_account/models/res_partner_bank.py
+++ mcfix_account/models/res_partner_bank.py
@@ -31,13 +31,12 @@ class ResPartnerBank(models.Model):
                           'Res Partner Bank is assigned to '
                           'Account Bank Statement Line (%s)'
                           '.' % field.name_get()[0][1]))
-                # Waiting merge of https://github.com/odoo/odoo/pull/23480
-                # field = self.env['account.journal'].search(
-                #     [('bank_account_id', '=', rec.id),
-                #      ('company_id', '!=', False),
-                #      ('company_id', '!=', rec.company_id.id)], limit=1)
-                # if field:
-                #     raise ValidationError(
-                #         _('You cannot change the company, as this '
-                #           'Res Partner Bank is assigned to Account Journal '
-                #           '(%s).' % field.name_get()[0][1]))
+                field = self.env['account.journal'].search(
+                    [('bank_account_id', '=', rec.id),
+                     ('company_id', '!=', False),
+                     ('company_id', '!=', rec.company_id.id)], limit=1)
+                if field:
+                    raise ValidationError(
+                        _('You cannot change the company, as this '
+                          'Res Partner Bank is assigned to Account Journal '
+                          '(%s).' % field.name_get()[0][1]))
