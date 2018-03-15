d5eb2f3b1049059b53c7f92dce997b03f1c23552
mreficent <miquel.raich@eficent.com>
2018-03-15 15:29:15 +0100

    [FIX] Several fixes in mcfix_base/account/stock

--- mcfix_account/models/account_payment.py
+++ mcfix_account/models/account_payment.py
@@ -24,9 +24,36 @@ class AccountPayment(models.Model):
     @api.onchange('payment_type', 'company_id')
     def _onchange_payment_type(self):
         res = super(AccountPayment, self)._onchange_payment_type()
-        res['domain']['journal_id'].append(
-            ('company_id', '=', self.company_id.id))
+        if self.invoice_ids:
+            res['domain']['journal_id'].append(
+                ('company_id', 'in',
+                 self.invoice_ids.mapped('company_id').ids))
+        else:
+            res['domain']['journal_id'].append(
+                ('company_id', '=', self.company_id.id))
         return res
+
+    @api.onchange('amount', 'currency_id')
+    def _onchange_amount(self):
+        journal_type = self.journal_id.type
+        super(AccountPayment, self)._onchange_amount()
+        jrnl_filters = self._compute_journal_domain_and_types()
+        journal_types = jrnl_filters['journal_types']
+        domain_on_types = [('type', 'in', list(journal_types))]
+
+        journal_domain = jrnl_filters['domain'] + domain_on_types
+        default_journal_id = self.env.context.get('default_journal_id')
+        domain_company = [('company_id', '=', self.env.context.get(
+            'default_company_id', self.env.user.company_id.id))]
+        if not default_journal_id:
+            if journal_type not in journal_types:
+                self.journal_id = self.env['account.journal'].search(
+                    domain_on_types + domain_company, limit=1)
+        else:
+            journal_domain = journal_domain.append(
+                ('id', '=', default_journal_id))
+
+        return {'domain': {'journal_id': journal_domain}}
 
     @api.model
     def create(self, vals):
