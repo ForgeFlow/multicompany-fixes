58bb954ad92ad5e9a38fbc796577be54a74bc48a
Enric Tobella <etobella@creublanca.es>
2018-03-28 11:29:49 +0200

    [FIX] sql-inject error on travis

--- mcfix_base/models/ir_default.py
+++ mcfix_base/models/ir_default.py
@@ -11,22 +11,23 @@ class IrDefault(models.Model):
         super(IrDefault, self).get_model_defaults(
             model_name=model_name, condition=condition)
         cr = self.env.cr
-        query = """ SELECT f.name, d.json_value FROM ir_default d
-                    JOIN ir_model_fields f ON d.field_id=f.id
-                    JOIN res_users u ON u.id=%s
-                    WHERE f.model=%s
-                        AND (d.user_id IS NULL OR d.user_id=u.id)
-                        AND (d.company_id IS NULL OR d.company_id={id1})
-                        AND {id2}
-                    ORDER BY d.user_id, d.company_id, d.id
-                """
+        query = 'SELECT f.name, d.json_value ' \
+                'FROM ir_default d ' \
+                'JOIN ir_model_fields f ON d.field_id=f.id ' \
+                'JOIN res_users u ON u.id=%s ' \
+                'WHERE ' \
+                'f.model=%s ' \
+                'AND (d.user_id IS NULL OR d.user_id=u.id) ' \
+                'AND (d.company_id IS NULL OR d.company_id=%d) ' \
+                'AND {condition} ' \
+                'ORDER BY d.user_id, d.company_id, d.id'
         company = self._context.get('default_company_id') or "u.company_id"
-        params = [self.env.uid, model_name]
+        params = [self.env.uid, model_name, company]
         if condition:
-            query = query.format(id1=company, id2="d.condition=%s")
+            query = query.format(condition="d.condition=%s")
             params.append(condition)
         else:
-            query = query.format(id1=company, id2="d.condition IS NULL")
+            query = query.format(condition="d.condition IS NULL")
         # pylint: disable=E8103
         cr.execute(query, params)
         result = {}
