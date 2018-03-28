dd1c23d95ca2905168a348d876fd4e478f3d162e
Enric Tobella <etobella@creublanca.es>
2018-03-28 21:57:42 +0200

    [REF][FIX] Several fixes and constrains improvement

--- mcfix_account_check_printing/models/ir_sequence.py
+++ mcfix_account_check_printing/models/ir_sequence.py
@@ -1,23 +1,12 @@
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class IrSequence(models.Model):
     _inherit = 'ir.sequence'
 
-    @api.constrains('company_id')
-    def _check_company_id_out_model(self):
-        super(IrSequence, self)._check_company_id_out_model()
-        if not self.env.context.get('bypass_company_validation', False):
-            for rec in self:
-                if not rec.company_id:
-                    continue
-                field = self.env['account.journal'].search(
-                    [('check_sequence_id', '=', rec.id),
-                     ('company_id', '!=', False),
-                     ('company_id', '!=', rec.company_id.id)], limit=1)
-                if field:
-                    raise ValidationError(
-                        _('You cannot change the company, as this '
-                          'Ir Sequence is assigned to Account Journal '
-                          '(%s).' % field.name_get()[0][1]))
+    def _check_company_id_search(self):
+        res = super()._check_company_id_search()
+        res += [
+            ('account.journal', [('check_sequence_id', '=', self.id)]),
+        ]
+        return res
