00b44dfa247f27695f0291ef8c31fd0f53bcb58b
Enric Tobella <etobella@creublanca.es>
2018-03-28 14:10:57 +0200

    fixup! Error on sequence

--- mcfix_account/models/ir_sequence.py
+++ mcfix_account/models/ir_sequence.py
@@ -4,8 +4,8 @@ from odoo import models
 class IrSequence(models.Model):
     _inherit = 'ir.sequence'
 
-    def chart_template_id(self):
-        res = super().chart_template_id()
+    def _check_company_id_search(self):
+        res = super()._check_company_id_search()
         res = res + [
             ('account.journal', [('refund_sequence_id', '=', self.id)]),
             ('account.journal', [('sequence_id', '=', self.id)]),
