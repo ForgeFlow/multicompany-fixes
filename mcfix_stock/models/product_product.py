2c63380b022f1358a1a9a7a13c28d38923e67e92
Enric Tobella <etobella@creublanca.es>
2018-03-28 21:57:42 +0200

    [REF][FIX] Several fixes and constrains improvement

--- mcfix_stock/models/product_product.py
+++ mcfix_stock/models/product_product.py
@@ -10,8 +10,7 @@ class ProductProduct(models.Model):
     def _check_company_id_route_ids(self):
         for rec in self.sudo():
             for line in rec.route_ids:
-                if rec.company_id and line.company_id and\
-                        rec.company_id != line.company_id:
+                if not line.check_company(rec.company_id):
                     raise ValidationError(
                         _('The Company in the Product Product and in '
                           'Stock Location Route (%s) must be the same.'
@@ -22,8 +21,7 @@ class ProductProduct(models.Model):
     def _check_company_id_route_from_categ_ids(self):
         for rec in self.sudo():
             for line in rec.route_from_categ_ids:
-                if rec.company_id and line.company_id and\
-                        rec.company_id != line.company_id:
+                if not line.check_company(rec.company_id):
                     raise ValidationError(
                         _('The Company in the Product Product and in '
                           'Stock Location Route (%s) must be the same.'
--- mcfix_stock/models/stock_inventory.py
+++ mcfix_stock/models/stock_inventory.py
@@ -7,29 +7,24 @@ class Inventory(models.Model):
 
     @api.onchange('company_id')
     def _onchange_company_id(self):
-        if self.company_id and self.product_id.company_id and \
-                self.product_id.company_id != self.company_id:
+        if not self.product_id.check_company(self.company_id):
             self.product_id = False
-        if self.company_id and self.location_id.company_id and \
-                self.location_id.company_id != self.company_id:
+        if not self.location_id.check_company(self.company_id):
             if self.product_id.location_id:
                 self.location_id = self.product_id.location_id
             else:
                 self._cache.update(self._convert_to_cache(
                     {'location_id': False}, update=True))
-        if self.company_id and self.partner_id.company_id and \
-                self.partner_id.company_id != self.company_id:
+        if not self.partner_id.check_company(self.company_id):
             self.partner_id = False
-        if self.company_id and self.package_id.company_id and \
-                self.package_id.company_id != self.company_id:
+        if not self.package_id.check_company(self.company_id):
             self.package_id = False
 
     @api.multi
     @api.constrains('company_id', 'location_id')
     def _check_company_id_location_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.location_id.company_id and \
-                    rec.company_id != rec.location_id.company_id:
+            if not rec.location_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory and in '
                       'Stock Location must be the same.'))
@@ -38,8 +33,7 @@ class Inventory(models.Model):
     @api.constrains('company_id', 'partner_id')
     def _check_company_id_partner_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.partner_id.company_id and \
-                    rec.company_id != rec.partner_id.company_id:
+            if not rec.partner_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory and in '
                       'Res Partner must be the same.'))
@@ -48,8 +42,7 @@ class Inventory(models.Model):
     @api.constrains('company_id', 'package_id')
     def _check_company_id_package_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.package_id.company_id and \
-                    rec.company_id != rec.package_id.company_id:
+            if not rec.package_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory and in '
                       'Stock Quant Package must be the same.'))
@@ -58,37 +51,19 @@ class Inventory(models.Model):
     @api.constrains('company_id', 'product_id')
     def _check_company_id_product_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.product_id.company_id and \
-                    rec.company_id != rec.product_id.company_id:
+            if not rec.product_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory and in '
                       'Product Product must be the same.'))
 
     @api.constrains('company_id')
     def _check_company_id_out_model(self):
-        if not self.env.context.get('bypass_company_validation', False):
-            for rec in self:
-                if not rec.company_id:
-                    continue
-                field = self.env['stock.inventory.line'].search(
-                    [('inventory_id', '=', rec.id),
-                     ('company_id', '!=', False),
-                     ('company_id', '!=', rec.company_id.id)], limit=1)
-                if field:
-                    raise ValidationError(
-                        _('You cannot change the company, as this '
-                          'Stock Inventory is assigned to '
-                          'Stock Inventory Line (%s)'
-                          '.' % field.name_get()[0][1]))
-                field = self.env['stock.move'].search(
-                    [('inventory_id', '=', rec.id),
-                     ('company_id', '!=', False),
-                     ('company_id', '!=', rec.company_id.id)], limit=1)
-                if field:
-                    raise ValidationError(
-                        _('You cannot change the company, as this '
-                          'Stock Inventory is assigned to Stock Move '
-                          '(%s).' % field.name_get()[0][1]))
+        self._check_company_id_base_model()
+
+    def _check_company_id_fields(self):
+        res = super()._check_company_id_fields()
+        res += [self.line_ids, self.move_ids, ]
+        return res
 
 
 class InventoryLine(models.Model):
@@ -98,8 +73,7 @@ class InventoryLine(models.Model):
     @api.constrains('company_id', 'location_id')
     def _check_company_id_location_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.location_id.company_id and \
-                    rec.company_id != rec.location_id.company_id:
+            if not rec.location_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory Line and in '
                       'Stock Location must be the same.'))
@@ -108,8 +82,7 @@ class InventoryLine(models.Model):
     @api.constrains('company_id', 'partner_id')
     def _check_company_id_partner_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.partner_id.company_id and \
-                    rec.company_id != rec.partner_id.company_id:
+            if not rec.partner_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory Line and in '
                       'Res Partner must be the same.'))
@@ -118,8 +91,7 @@ class InventoryLine(models.Model):
     @api.constrains('company_id', 'inventory_id')
     def _check_company_id_inventory_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.inventory_id.company_id and \
-                    rec.company_id != rec.inventory_id.company_id:
+            if not rec.inventory_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory Line and in '
                       'Stock Inventory must be the same.'))
@@ -128,8 +100,7 @@ class InventoryLine(models.Model):
     @api.constrains('company_id', 'package_id')
     def _check_company_id_package_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.package_id.company_id and \
-                    rec.company_id != rec.package_id.company_id:
+            if not rec.package_id.check_company(rec.company_id):
                 raise ValidationError(
                     _('The Company in the Stock Inventory Line and in '
                       'Stock Quant Package must be the same.'))
@@ -138,8 +109,7 @@ class InventoryLine(models.Model):
     # @api.constrains('company_id', 'product_id')
     # def _check_company_id_product_id(self):
     #     for rec in self.sudo():
-    #         if rec.company_id and rec.product_id.company_id and \
-    #                 rec.company_id != rec.product_id.company_id:
+    #         if not rec.product_id.check_company(rec.company_id):
     #             raise ValidationError(
     #                 _('The Company in the Stock Inventory Line and in '
     #                   'Product Product must be the same.'))
