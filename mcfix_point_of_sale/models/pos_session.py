dc120969d3d3776b1f5f917a89f4aab6ec61f2b5
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-04-09 12:01:44 +0200

    fixes and tests added to mcfix_point_of_sale

--- mcfix_point_of_sale/models/pos_session.py
+++ mcfix_point_of_sale/models/pos_session.py
@@ -1,12 +1,27 @@
-from odoo import api, models, _
+# Copyright 2018 Creu Blanca
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
+    company_id = fields.Many2one('res.company', string='Company',
+                                 related='config_id.company_id', store=True)
+
+    def _check_company_id_search(self):
+        res = super()._check_company_id_search()
+        res += [
+            ('account.bank.statement', [('pos_session_id', '=', self.id)]),
+        ]
+        return res
+
     @api.model
     def create(self, values):
+        # Here we have to redefine the logic to obtain pos_config.journal_ids
+        # because in the core odoo it does not take into account the company.
         config_id = values.get('config_id') or \
             self.env.context.get('default_config_id')
         if not config_id:
--- mcfix_point_of_sale/tests/test_point_of_sale.py
+++ mcfix_point_of_sale/tests/test_point_of_sale.py
@@ -1,14 +1,22 @@
+# Copyright 2018 Creu Blanca
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 import logging
-from odoo.tests.common import TransactionCase
 from odoo.exceptions import ValidationError
+from odoo import fields
+from odoo.tools import mute_logger
+from odoo.addons.mcfix_account.tests.test_account_chart_template_consistency \
+    import TestAccountChartTemplate
 
 _logger = logging.getLogger(__name__)
 
 
-class TestPointOfSale(TransactionCase):
+class TestPointOfSale(TestAccountChartTemplate):
+
+    def with_context(self, *args, **kwargs):
+        context = dict(args[0] if args else self.env.context, **kwargs)
+        self.env = self.env(context=context)
+        return self
 
     def setUp(self):
         super(TestPointOfSale, self).setUp()
@@ -17,17 +25,18 @@ class TestPointOfSale(TransactionCase):
         pos_manager_group = self.env.ref('point_of_sale.group_pos_manager')
         self.pos_model = self.env['pos.order']
         self.journal_model = self.env['account.journal']
+
+        self.fruits_vegetables = self.env.ref(
+            'point_of_sale.fruits_vegetables')
+        self.carotte = self.env.ref('point_of_sale.carotte')
+        self.courgette = self.env.ref('point_of_sale.courgette')
+        self.onions = self.env.ref('point_of_sale.Onions')
+
         manager_pos_test_group = self.create_full_access(
             ['pos.order', 'pos.order.line', 'product.pricelist',
              'product.pricelist.item', 'account.journal', 'account.move',
              'account.move.line', 'account.reconcile.model',
              'account.payment', 'account.asset.category'])
-        self.company = self.env['res.company'].create(
-            self.create_company('Test company')
-        )
-        self.company_2 = self.env['res.company'].create(
-            self.create_company('Test company 2', self.company.id)
-        )
         self.env.user.company_ids += self.company
         self.env.user.company_ids += self.company_2
 
@@ -57,16 +66,36 @@ class TestPointOfSale(TransactionCase):
             'company_id': self.company.id,
         })
 
-        self.pos_config = self.env['pos.config'].sudo(self.user).create({
+        self.pos_config = self.env['pos.config'].create({
             'name': 'Config Test',
             'company_id': self.company.id,
             'journal_id': self.sale_journal.id,
+            'journal_ids': [(0, 0, {'name': 'Cash Journal - Test',
+                                    'code': 'TSC',
+                                    'type': 'cash',
+                                    'company_id': self.company.id,
+                                    'journal_user': True})],
         })
 
-        self.pos_session = self.env['pos.session'].sudo(self.user).create({
-            'user_id': self.user.id,
-            'config_id': self.pos_config.id,
+        self.income_account = self.env['account.account'].create({
+            'company_id': self.company.id,
+            'code': 'INC1',
+            'name': 'Income',
+            'user_type_id': self.env.ref(
+                'account.data_account_type_revenue').id
         })
+
+        self.carotte.with_context(
+            force_company=self.company_id).property_account_income_id = \
+            self.income_account
+
+        self.onions.with_context(
+            force_company=self.company_id).property_account_income_id = \
+            self.income_account
+
+        self.courgette.with_context(
+            force_company=self.company_id).property_account_income_id = \
+            self.income_account
 
     def create_full_access(self, list_of_models):
         manager_pos_test_group = self.env['res.groups'].sudo().create({
@@ -94,12 +123,234 @@ class TestPointOfSale(TransactionCase):
         }
         return dict_company
 
+    def test_pos_config_create(self):
+        pos_config = self.env['pos.config'].sudo(self.user).create({
+            'name': 'Config Test',
+            'company_id': self.company.id,
+        })
+        self.assertEquals(pos_config.sequence_id.company_id,
+                          pos_config.company_id)
+        self.assertEquals(pos_config.sequence_line_id.company_id,
+                          pos_config.company_id)
+
+    def test_pos_config_onchange(self):
+        pos_config = self.env['pos.config'].sudo(self.user).new({
+            'name': 'Config Test',
+            'company_id': self.company.id,
+            'journal_id': self.sale_journal.id,
+            'journal_ids': [(0, 0, {'name': 'Cash Journal - Test',
+                                    'code': 'TSC',
+                                    'type': 'cash',
+                                    'company_id': self.company.id,
+                                    'journal_user': True})],
+        })
+
+        pos_config.company_id = self.company_2
+        pos_config._onchange_company_id()
+        self.assertNotEquals(pos_config.journal_id, self.sale_journal)
+        for journal in pos_config.journal_ids:
+            self.assertNotEquals(journal.company_id, self.company)
+
     def test_constrains(self):
+        pos_session = self.env['pos.session'].sudo(self.user).create({
+            'user_id': self.user.id,
+            'config_id': self.pos_config.id,
+        })
         self.pos_1 = self.pos_model.sudo(self.user).create({
             'name': 'Pos - Test',
             'pricelist_id': self.pricelist.id,
             'company_id': self.company.id,
-            'session_id': self.pos_session.id,
+            'session_id': pos_session.id,
         })
         with self.assertRaises(ValidationError):
             self.pos_1.company_id = self.company_2
+
+    def test_create_from_ui(self):
+        """
+        Simulation of sales coming from the interface,
+        even after closing the session
+        """
+        fromproduct = object()
+
+        def compute_tax(product, price, taxes=fromproduct, qty=1):
+            if taxes is fromproduct:
+                taxes = product.taxes_id
+            currency = self.pos_config.pricelist_id.currency_id
+            taxes = taxes.compute_all(price, currency, qty,
+                                      product=product)['taxes']
+            untax = price * qty
+            return untax, sum(tax.get('amount', 0.0) for tax in taxes)
+
+        # I click on create a new session button
+        self.pos_config.open_session_cb()
+
+        current_session = self.pos_config.current_session_id
+        num_starting_orders = len(current_session.order_ids)
+
+        untax, atax = compute_tax(self.carotte, 0.9)
+        carrot_order = {
+            'data':
+                {'amount_paid': untax + atax,
+                 'amount_return': 0,
+                 'amount_tax': atax,
+                 'amount_total': untax + atax,
+                 'creation_date': fields.Datetime.now(),
+                 'fiscal_position_id': False,
+                 'pricelist_id': self.pos_config.available_pricelist_ids[0].id,
+                 'lines': [
+                     [0, 0,
+                      {'discount': 0,
+                       'id': 42,
+                       'pack_lot_ids': [],
+                       'price_unit': 0.9,
+                       'product_id': self.carotte.id,
+                       'qty': 1,
+                       'tax_ids': [(6, 0, self.carotte.taxes_id.ids)]}
+                      ]
+                 ],
+                 'name': 'Order 00042-003-0014',
+                 'partner_id': False,
+                 'pos_session_id': current_session.id,
+                 'sequence_number': 2,
+                 'statement_ids': [
+                     [0, 0,
+                      {'account_id':
+                       self.env.user.partner_id.with_context(
+                           force_company=self.company.id
+                       ).property_account_receivable_id.id,
+                       'amount': untax + atax,
+                       'journal_id': self.pos_config.journal_ids[0].id,
+                       'name': fields.Datetime.now(),
+                       'statement_id': current_session.statement_ids[0].id}]],
+                 'uid': '00042-003-0014',
+                 'user_id': self.env.uid
+                 },
+            'id': '00042-003-0014',
+            'to_invoice': False}
+
+        untax, atax = compute_tax(self.courgette, 1.2)
+        zucchini_order = {
+            'data':
+                {'amount_paid': untax + atax,
+                 'amount_return': 0,
+                 'amount_tax': atax,
+                 'amount_total': untax + atax,
+                 'creation_date': fields.Datetime.now(),
+                 'fiscal_position_id': False,
+                 'pricelist_id': self.pos_config.available_pricelist_ids[0].id,
+                 'lines': [
+                     [0, 0,
+                      {'discount': 0,
+                       'id': 3,
+                       'pack_lot_ids': [],
+                       'price_unit': 1.2,
+                       'product_id': self.courgette.id,
+                       'qty': 1,
+                       'tax_ids': [(6, 0, self.courgette.taxes_id.ids)]}]],
+                 'name': 'Order 00043-003-0014',
+                 'partner_id': False,
+                 'pos_session_id': current_session.id,
+                 'sequence_number': self.pos_config.journal_id.id,
+                 'statement_ids': [
+                     [0, 0,
+                      {'account_id':
+                       self.env.user.partner_id.with_context(
+                           force_company=self.company.id
+                       ).property_account_receivable_id.id,
+                       'amount': untax + atax,
+                       'journal_id': self.pos_config.journal_ids[0].id,
+                       'name': fields.Datetime.now(),
+                       'statement_id': current_session.statement_ids[0].id
+                       }
+                      ]
+                 ],
+                 'uid': '00043-003-0014',
+                 'user_id': self.env.uid},
+            'id': '00043-003-0014',
+            'to_invoice': False}
+
+        untax, atax = compute_tax(self.onions, 1.28)
+        onions_order = {
+            'data': {'amount_paid': untax + atax,
+                     'amount_return': 0,
+                     'amount_tax': atax,
+                     'amount_total': untax + atax,
+                     'creation_date': fields.Datetime.now(),
+                     'fiscal_position_id': False,
+                     'pricelist_id': self.pos_config.available_pricelist_ids[
+                         0].id,
+                     'lines': [
+                         [0, 0,
+                          {'discount': 0,
+                           'id': 3,
+                           'pack_lot_ids': [],
+                           'price_unit': 1.28,
+                           'product_id': self.onions.id,
+                           'qty': 1,
+                           'tax_ids': [[6, False,
+                                        self.onions.taxes_id.ids]]
+                           }
+                          ]
+                     ],
+                     'name': 'Order 00044-003-0014',
+                     'partner_id': False,
+                     'pos_session_id': current_session.id,
+                     'sequence_number': self.pos_config.journal_id.id,
+                     'statement_ids': [
+                         [0, 0,
+                          {'account_id':
+                           self.env.user.partner_id.with_context(
+                               force_company=self.company.id
+                           ).property_account_receivable_id.id,
+                           'amount': untax + atax,
+                           'journal_id': self.pos_config.journal_ids[0].id,
+                           'name': fields.Datetime.now(),
+                           'statement_id': current_session.statement_ids[0].id
+                           }
+                          ]
+                     ],
+                     'uid': '00044-003-0014',
+                     'user_id': self.env.uid
+                     },
+            'id': '00044-003-0014',
+            'to_invoice': False}
+
+        # I create an order on an open session
+        self.pos_model.create_from_ui([carrot_order])
+        self.assertEqual(num_starting_orders + 1, len(
+            current_session.order_ids), "Submitted order not encoded")
+
+        # I resubmit the same order
+        self.pos_model.create_from_ui([carrot_order])
+        self.assertEqual(num_starting_orders + 1, len(
+            current_session.order_ids), "Resubmitted order was not skipped")
+
+        # I close the session
+        current_session.action_pos_session_closing_control()
+        self.assertEqual(current_session.state, 'closed',
+                         "Session was not properly closed")
+        self.assertFalse(self.pos_config.current_session_id,
+                         "Current session not properly recomputed")
+
+        # I keep selling after the session is closed
+        with mute_logger('odoo.addons.point_of_sale.models.pos_order'):
+            self.pos_model.create_from_ui([zucchini_order, onions_order])
+        rescue_session = self.env['pos.session'].search([
+            ('config_id', '=', self.pos_config.id),
+            ('state', '=', 'opened'),
+            ('rescue', '=', True)
+        ])
+        self.assertEqual(
+            len(rescue_session), 1,
+            "One (and only one) rescue session should "
+            "be created for orphan orders")
+        self.assertIn("(RESCUE FOR %s)" % current_session.name,
+                      rescue_session.name,
+                      "Rescue session is not linked to the previous one")
+        self.assertEqual(len(rescue_session.order_ids), 2,
+                         "Rescue session does not contain both orders")
+
+        # I close the rescue session
+        rescue_session.action_pos_session_closing_control()
+        self.assertEqual(rescue_session.state, 'closed',
+                         "Rescue session was not properly closed")
