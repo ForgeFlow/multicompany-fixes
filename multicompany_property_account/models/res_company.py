b79533e3b7dbcdaf15d990c8f76b5c75cd5f1110
Enric Tobella <etobella@creublanca.es>
2018-04-11 13:23:50 +0200

    [IMP] Add company fields for accounts

--- multicompany_property_account/models/res_company.py
+++ multicompany_property_account/models/res_company.py
@@ -49,6 +49,121 @@ class ResCompany(models.Model):
     property_stock_valuation_account_id = fields.Many2one(
         domain="[('company_id', '=', active_id)]",
     )
+    partner_account_payable_id = fields.Many2one(
+        'account.account',
+        domain="[('internal_type', '=', 'payable'),"
+               "('deprecated', '=', False),"
+               "('company_id', '=', active_id)]",
+        compute='_compute_partner_account_payable',
+        inverse='_inverse_partner_account_payable',
+    )
+    partner_account_receivable_id = fields.Many2one(
+        'account.account',
+        domain="[('internal_type', '=', 'receivable'),"
+               "('deprecated', '=', False),"
+               "('company_id', '=', active_id)]",
+        compute='_compute_partner_account_receivable',
+        inverse='_inverse_partner_account_receivable',
+    )
+    categ_account_expense_id = fields.Many2one(
+        'account.account',
+        domain="[('internal_type', '=', 'expense'),"
+               "('deprecated', '=', False),"
+               "('company_id', '=', active_id)]",
+        compute='_compute_categ_account_expense',
+        inverse='_inverse_categ_account_expense',
+    )
+    categ_account_income_id = fields.Many2one(
+        'account.account',
+        domain="[('internal_type', '=', 'income'),"
+               "('deprecated', '=', False),"
+               "('company_id', '=', active_id)]",
+        compute='_compute_categ_account_income',
+        inverse='_inverse_categ_account_income',
+    )
+
+    def get_property_value(self, model, field):
+        value = self.env['ir.property'].with_context(
+            force_company=self.id).sudo().get(field, model)
+        if value:
+            if isinstance(value, list):
+                return value[0]
+            else:
+                return value
+        return False
+
+    def set_property_value(self, model, field_name, value):
+        field = self.env['ir.model.fields'].sudo().search([
+            ('name', '=', field_name),
+            ('model', '=', model)
+        ], limit=1)
+        val = value.id if isinstance(value, models.BaseModel) else value
+        self.env['ir.property'].sudo().search([
+            ('name', '=', field_name),
+            ('fields_id', '=', field.id),
+            ('company_id', '=', self.id),
+            ('res_id', '=', False)
+        ]).write({'value': val})
+
+    @api.model
+    def _compute_partner_account_payable(self):
+        for rec in self:
+            rec.partner_account_payable_id = rec.get_property_value(
+                'res.partner', 'property_account_payable_id'
+            )
+
+    @api.model
+    def _inverse_partner_account_payable(self):
+        for rec in self:
+            rec.set_property_value(
+                'res.partner', 'property_account_payable_id',
+                rec.partner_account_payable_id
+            )
+
+    @api.model
+    def _compute_partner_account_receivable(self):
+        for rec in self:
+            rec.partner_account_receivable_id = rec.get_property_value(
+                'res.partner', 'property_account_receivable_id'
+            )
+
+    @api.model
+    def _inverse_partner_account_receivable(self):
+        for rec in self:
+            rec.set_property_value(
+                'res.partner', 'property_account_receivable_id',
+                rec.partner_account_receivable_id
+            )
+
+    @api.model
+    def _compute_categ_account_expense(self):
+        for rec in self:
+            rec.categ_account_expense_id = rec.get_property_value(
+                'product.category', 'property_account_expense_categ_id'
+            )
+
+    @api.model
+    def _inverse_categ_account_expense(self):
+        for rec in self:
+            rec.set_property_value(
+                'product.category', 'property_account_expense_categ_id',
+                rec.categ_account_expense_id
+            )
+
+    @api.model
+    def _compute_categ_account_income(self):
+        for rec in self:
+            rec.categ_account_income_id = rec.get_property_value(
+                'product.category', 'property_account_income_categ_id'
+            )
+
+    @api.model
+    def _inverse_categ_account_income(self):
+        for rec in self:
+            rec.set_property_value(
+                'product.category', 'property_account_income_categ_id',
+                rec.categ_account_income_id
+            )
 
     @api.model
     def _compute_tax(self):
