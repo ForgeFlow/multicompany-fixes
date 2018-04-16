377092536acb3af1f14e09724b9290fcf01d5bb1
Enric Tobella <etobella@creublanca.es>
2018-04-16 10:56:06 +0200

    [ADD] multicompany_property_account_payment_partner

---
+++ multicompany_property_account_payment_partner/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Multi Company Account Payment',
+    'version': '11.0.1.0.0',
+    'summary': 'Account Payment Company Properties',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'sequence': 30,
+    'license': 'LGPL-3',
+    'website': 'http://www.eficent.com',
+    'depends': ['multicompany_property_account',
+                'account_payment_partner'],
+    'data': [
+        'views/partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
