3c8896b19788ca06b47be60f1446cfca485c9013
Enric Tobella <etobella@creublanca.es>
2018-04-26 12:35:50 +0200

    [FIX] Remove unrelation of company on account move

--- multicompany_property_account_payment_partner/__manifest__.py
+++ multicompany_property_account_payment_partner/__manifest__.py
@@ -3,13 +3,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Multi Company Account Payment',
+    'name': 'Multi Company Account Payment Partner',
     'version': '11.0.1.0.0',
     'summary': 'Account Payment Company Properties',
     'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
     'sequence': 30,
     'license': 'LGPL-3',
-    'website': 'http://www.eficent.com',
+    'website': 'https://github.com/OCA/multi-company',
     'depends': ['multicompany_property_account',
                 'account_payment_partner'],
     'data': [
