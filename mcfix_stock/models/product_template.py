9171c3df3e7035ccde1d593415fe4964f47ce0aa
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-05-17 12:44:30 +0200

    do not check constrains on non-stored models

--- mcfix_stock/models/product_template.py
+++ mcfix_stock/models/product_template.py
@@ -5,14 +5,6 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.multi
-    @api.constrains('company_id', 'warehouse_id')
-    def _check_company_id_warehouse_id(self):
-        for rec in self.sudo():
-            if not rec.warehouse_id.check_company(rec.company_id):
-                raise ValidationError(
-                    _('The Company in the Product Template and in '
-                      'Stock Warehouse must be the same.'))
 
     @api.multi
     @api.constrains('company_id')
@@ -24,15 +16,6 @@ class ProductTemplate(models.Model):
                         _('The Company in the Product Template and in '
                           'Stock Location Route (%s) must be the same.'
                           ) % line.name_get()[0][1])
-
-    @api.multi
-    @api.constrains('company_id', 'location_id')
-    def _check_company_id_location_id(self):
-        for rec in self.sudo():
-            if not self.location_id.check_company(rec.company_id):
-                raise ValidationError(
-                    _('The Company in the Product Template and in '
-                      'Stock Location must be the same.'))
 
     @api.multi
     @api.constrains('company_id', 'route_ids')
@@ -47,8 +30,7 @@ class ProductTemplate(models.Model):
 
     def _check_company_id_fields(self):
         res = super()._check_company_id_fields()
-        res += [self.route_ids, self.route_from_categ_ids,
-                self.location_id, self.warehouse_id]
+        res += [self.route_ids, self.route_from_categ_ids, ]
         return res
 
     def _check_company_id_search(self):
