ff86e1fc13e617a8db452df289f8cd6ddada81a2
Enric Tobella <etobella@creublanca.es>
2019-03-11 23:50:08 +0100

    [FIX] point_of_sale constrians

--- mcfix_point_of_sale/models/account_bank_statement.py
+++ mcfix_point_of_sale/models/account_bank_statement.py
@@ -5,19 +5,6 @@ from odoo import api, models, _
 from odoo.exceptions import ValidationError
 
 
-class AccountBankStatement(models.Model):
-    _inherit = 'account.bank.statement'
-
-    @api.multi
-    @api.constrains('company_id', 'pos_session_id')
-    def _check_company_id_pos_session_id(self):
-        for rec in self.sudo():
-            if not rec.pos_session_id.check_company(rec.company_id):
-                raise ValidationError(
-                    _('The Company in the Account Bank Statement Line and in '
-                      'POS Session must be the same.'))
-
-
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
 
