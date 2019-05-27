30fc2bcc7364de6e13d4cfb7a7e79175aa4bc1bc
Enric Tobella <etobella@creublanca.es>
2019-05-27 19:20:42 +0200

    [MIG] mcfix_stock_account

--- mcfix_stock_account/__manifest__.py
+++ mcfix_stock_account/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Multi Company Fix Stock Account',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'summary': 'Stock Account fixes',
     'author': 'Eficent, Odoo Community Association (OCA)',
     'website': 'http://www.eficent.com',
