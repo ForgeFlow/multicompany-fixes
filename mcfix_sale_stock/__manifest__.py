ee06d4cecc8492fd6f6b80abbb25d3f6d71ce361
Enric Tobella <etobella@creublanca.es>
2019-05-27 19:23:53 +0200

    [MIG] mcfix_sale_stock

--- mcfix_sale_stock/__manifest__.py
+++ mcfix_sale_stock/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Multi Company Fix Sale Stock',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'summary': 'Sale Stock fixes',
     'author': 'Eficent, Odoo Community Association (OCA)',
     'website': 'http://www.eficent.com',
