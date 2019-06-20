b206f3ba773362f121689940d14cfaeabd5e704d
Aaron Henriquez <ahenriquez@eficent.com>
2019-06-20 15:29:46 +0200

    Split multicompany_property_account. Module multicompany_property_account_base for exclusive company properties

--- multicompany_property_account/models/__init__.py
+++ multicompany_property_account/models/__init__.py
@@ -1,5 +1,4 @@
 from . import chart_template
-from . import res_company
 from . import res_partner
 from . import product
 from . import product_category
--- multicompany_property_account/tests/__init__.py
+++ multicompany_property_account/tests/__init__.py
@@ -1,2 +1,1 @@
 from . import test_multicompany
-from . import test_account_company_config
