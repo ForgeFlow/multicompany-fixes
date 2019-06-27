977aebd4e79fdaab5b8cd6518585228752e1f421
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-27 12:49:06 +0200

    [12.0][MIG] mcfix account

--- mcfix_account_check_printing/__manifest__.py
+++ mcfix_account_check_printing/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Multi Company Fix Account Check Printing',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'summary': 'Account Check Printing fixes',
     'author': 'Eficent, Odoo Community Association (OCA)',
     'website': 'http://www.eficent.com',
