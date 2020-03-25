46cad2679917d9975203098033ede2720dd1be38
Lois Rilo <lois.rilo@forgeflow.com>
2020-03-25 13:10:38 +0100

    [11.0][ADD] mcfix_base_multi_company

---
+++ mcfix_base_multi_company/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Mcfix Multi Company",
+    "version": "11.0.1.0.0",
+    "summary": "Integration of multicompany fixes and OCA multi company modules.",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/multicompany-fixes",
+    "license": "LGPL-3",
+    "depends": ["mcfix_base", "base_multi_company"],
+    "sequence": 150,
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
