58b5e8e44f3138c9c30978c06286e734a622dcca
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-31 08:30:15 +0200

    [12.0][FIX] report function

--- mcfix_account/report/account_reports.py
+++ mcfix_account/report/account_reports.py
@@ -5,7 +5,7 @@ class ReportJournal(models.AbstractModel):
     _inherit = 'report.account.report_journal'
 
     @api.model
-    def get_report_values(self, docids, data=None):
+    def _get_report_values(self, docids, data=None):
         res = super(ReportJournal, self).\
             get_report_values(docids=docids, data=data)
         res['company_id'] = self.env['res.company'].browse(
@@ -17,7 +17,7 @@ class ReportAgedPartnerBalance(models.AbstractModel):
     _inherit = 'report.account.report_agedpartnerbalance'
 
     @api.model
-    def get_report_values(self, docids, data=None):
+    def _get_report_values(self, docids, data=None):
         res = super(ReportAgedPartnerBalance, self).\
             get_report_values(docids=docids, data=data)
         res['company_id'] = self.env['res.company'].browse(
--- mcfix_point_of_sale/report/pos_order.py
+++ mcfix_point_of_sale/report/pos_order.py
@@ -8,7 +8,7 @@ class ReportSaleDetails(models.AbstractModel):
     _inherit = 'report.point_of_sale.report_saledetails'
 
     @api.multi
-    def get_report_values(self, docids, data=None):
+    def _get_report_values(self, docids, data=None):
         res = super(ReportSaleDetails, self).get_report_values(
             docids=docids, data=data)
         res['company_id'] = self.env['res.company'].browse(data['company_id'])
