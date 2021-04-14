4367e64dc4666525dc6b48302d6f4e755dca4e38
mreficent <miquel.raich@eficent.com>
2021-04-14 18:13:34 +0200

    multicompany_property_*: full update to v13

--- multicompany_property_stock_account/__manifest__.py
+++ multicompany_property_stock_account/__manifest__.py
@@ -1,23 +1,22 @@
 # Copyright 2017 Creu Blanca
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Multi Company Stock Account',
-    'version': '12.0.1.0.0',
-    'summary': 'Stock Account Company Properties',
-    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
-    'sequence': 30,
-    'license': 'LGPL-3',
-    'website': 'http://www.eficent.com',
-    'depends': ['multicompany_property_account',
-                'multicompany_property_stock'],
-    'data': [
-        'views/product_views.xml',
-        'views/product_category_views.xml',
-        'views/res_company_views.xml'
+    "name": "Multi Company Stock Account",
+    "version": "13.0.1.0.0",
+    "summary": "Stock Account Company Properties",
+    "author": "Creu Blanca, ForgeFlow, Odoo Community Association (OCA)",
+    "sequence": 30,
+    "license": "LGPL-3",
+    "website": "http://www.forgeflow.com",
+    "depends": ["multicompany_property_account", "multicompany_property_stock"],
+    "data": [
+        "views/product_views.xml",
+        "views/product_category_views.xml",
+        "views/res_company_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': True,
+    "installable": True,
+    "application": False,
+    "auto_install": True,
 }
