037bb59565f5527d0e3f23cb74a6a2f14a1af629
mreficent <miquel.raich@eficent.com>
2021-04-14 18:13:34 +0200

    multicompany_property_*: full update to v13

--- multicompany_property_stock/tests/test_multicompany.py
+++ multicompany_property_stock/tests/test_multicompany.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Creu Blanca
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.addons.multicompany_property_product.tests import test_multicompany
