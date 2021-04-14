b7f7bc849879b78f61322b48f50b8f332c05b44c
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-04-14 08:59:52 +0200

    Migrate multicompany_property modules to 13.0

--- multicompany_property_base/models/abstract.py
+++ multicompany_property_base/models/abstract.py
@@ -2,29 +2,24 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models, fields, api, _
+from odoo import _, fields, models
 from odoo.exceptions import MissingError
 
 
 class ModelProperty(models.AbstractModel):
-    _name = 'model.property'
-    _description = 'model property'
+    _name = "model.property"
+    _description = "model property"
 
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
-        required=True,
-        readonly=True
+        comodel_name="res.company", string="Company", required=True, readonly=True
     )
 
-    @api.multi
     def _compute_property_fields(self):
         """ This method must be redefined by modules that
         introduce property fields in the relevant models.
         If it is not defined by the models, will raise an error message."""
-        raise MissingError(_('It must be redefined'))
+        raise MissingError(_("It must be redefined"))
 
-    @api.multi
     def get_property_fields(self, object, properties):
         """ This method must be redefined by modules that
         introduce property fields in the relevant models.
@@ -40,7 +35,7 @@ class ModelProperty(models.AbstractModel):
 
         @returns: Does not expect to return anything
         """
-        raise MissingError(_('It must be redefined'))
+        raise MissingError(_("It must be redefined"))
 
     def set_property(self, object, fieldname, value, properties):
         """ This method will set the intended value
@@ -54,9 +49,9 @@ class ModelProperty(models.AbstractModel):
         the context the company for which we intend to save the value
         for the given object.
         """
-        properties.with_context(
-            force_company=self.company_id.id).sudo().set_multi(
-            fieldname, object._name, {object.id: value})
+        properties.with_context(force_company=self.company_id.id).sudo().set_multi(
+            fieldname, object._name, {object.id: value}
+        )
 
     def get_property_value(self, field, object, prop_obj):
         """ This method will assist in obtaining the value of a property
@@ -70,8 +65,7 @@ class ModelProperty(models.AbstractModel):
         the company for which we intend to save the value for
         the given object.
         """
-        value = prop_obj.get(field, object._name, (object._name + ',%s') %
-                             object.id)
+        value = prop_obj.get(field, object._name, (object._name + ",%s") % object.id)
         if value:
             if isinstance(value, list):
                 return value[0]
@@ -85,7 +79,6 @@ class ModelProperty(models.AbstractModel):
                 return value
         return False
 
-    @api.multi
     def get_property_fields_list(self):
         """ This should be implemented by the models that implement this
         abstract method, providing the list of property fields that the model
