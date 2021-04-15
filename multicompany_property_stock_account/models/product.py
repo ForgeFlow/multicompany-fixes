8253567bd3862ae828a63316e3670f978890afb1
mreficent <miquel.raich@eficent.com>
2021-04-15 10:28:31 +0200

    [FIX] multicompany_property_stock_account: property_cost_method had obsolete options

--- multicompany_property_stock_account/models/product.py
+++ multicompany_property_stock_account/models/product.py
@@ -33,16 +33,15 @@ class ProductProperty(models.TransientModel):
 
     property_cost_method = fields.Selection([
         ('standard', 'Standard Price'),
-        ('average', 'Average Price'),
-        ('real', 'Real Price')], string='Costing Method',
+        ('fifo', 'First In First Out (FIFO)'),
+        ('average', 'Average Cost (AVCO)')], string='Costing Method',
         compute='_compute_property_fields',
         readonly=False,
-        help="""Standard Price: The cost price is manually updated
-        at the end of a specific period (usually once a year).
-        Average Price: The cost price is recomputed at each incoming shipment
-        and used for the product valuation. Real Price: The cost price
-        displayed is the price of the last outgoing product (will be use in
-        case of inventory loss for example).""")
+        help="""Standard Price: The products are valued at their standard
+        cost defined on the product. Average Cost (AVCO): The products are
+        valued at weighted average cost. First In First Out (FIFO): The
+        products are valued supposing those that enter the company first
+        will also leave it first.""")
     property_stock_account_input = fields.Many2one(
         'account.account', 'Stock Input Account',
         domain=[('deprecated', '=', False)],
