ad6462ddc52957a00cc25812fed5f4d1da954511
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2022-01-11 12:07:26 +0100

    [13.0][ADD] multicompany_property_account_product_analytic

---
+++ multicompany_property_account_product_analytic/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ForgeFlow, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Multi Company Account Product Analytic",
+    "version": "13.0.1.0.0",
+    "summary": "Product Analytic Company Properties",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "sequence": 30,
+    "license": "AGPL-3",
+    "website": "http://www.forgeflow.com",
+    "depends": ["multicompany_property_account", "product_analytic"],
+    "data": ["views/product_views.xml", "views/product_category_views.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
