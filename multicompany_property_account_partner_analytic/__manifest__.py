72cbc96f68e82f9a74d1a298da4c3142cefa8034
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2023-05-29 13:14:36 +0200

    [ADD]multicompany_property_account_partner_analytic Add partner analytic account properties.

---
+++ multicompany_property_account_partner_analytic/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ForgeFlow, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Multi Company Account Partner Analytic",
+    "version": "13.0.1.0.0",
+    "summary": "Partner Analytic Company Properties",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/ForgeFlow/multicompany-fixes",
+    "depends": ["multicompany_property_account", "partner_analytic"],
+    "data": ["views/partner_views.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
