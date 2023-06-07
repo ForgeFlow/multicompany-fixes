9a9d1737d1fdc9686e3c014a9cd78140fa6f577f
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2023-06-07 10:13:06 +0200

    [FIX]multicompany_property_base: sync commercial fields with children for properties Properties are now not transfered to the child just as Odoo standard does. This is because when changing the value for a company the field updated on the partner is property_ids, but the fields being updated are not actually treated as such.

--- multicompany_property_base/models/res_partner.py
+++ multicompany_property_base/models/res_partner.py
@@ -32,6 +32,32 @@ class Partner(models.Model):
                 values.append(val.id)
             record.property_ids = values
 
+    def _children_sync(self, values):
+        """
+        Convert property values to the actual fields set on the partner, so that the
+        children contacts inherit the values correctly just as Odoo standard does.
+        """
+        if "property_ids" in values:
+            property_obj = self.env["res.partner.property"]
+            comm_fields = set(self._commercial_fields())
+            all_property_vals = values.get("property_ids")
+            for p_vals in all_property_vals:
+                is_list = isinstance(p_vals, list)
+                if not is_list or (is_list and len(p_vals) != 3):
+                    continue
+                p_id = p_vals[1]
+                p_val = p_vals[2]
+                prop = property_obj.browse(p_id)
+                if not p_val or not prop.exists():
+                    continue
+                company_id = prop.company_id.id
+                fields_to_sync = set(p_val.keys()) & comm_fields
+                if fields_to_sync:
+                    vals = {f: p_val.get(f) for f in fields_to_sync}
+                    self.with_context(force_company=company_id)._children_sync(vals)
+            values.pop("property_ids")
+        return super()._children_sync(values)
+
 
 class PartnerProperty(models.TransientModel):
     _name = "res.partner.property"
