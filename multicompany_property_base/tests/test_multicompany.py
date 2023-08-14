617d0911ee1eb0e35444de96678c307b19d6644c
Miquel Raïch <miquel.raich@forgeflow.com>
2023-08-14 11:42:30 +0200

    [FIX] multicompany_property_base: don't break tests

--- multicompany_property_base/tests/test_multicompany.py
+++ multicompany_property_base/tests/test_multicompany.py
@@ -6,21 +6,28 @@ from odoo.tests import TransactionCase
 
 
 class TestMulticompanyProperty(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.company_1 = self.create_company("company 1")
-        self.company_2 = self.create_company("company 2")
-        self.partner = self.env["res.partner"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company_1 = cls.create_company("company 1")
+        cls.company_2 = cls.create_company("company 2")
+        cls.partner = cls.env["res.partner"].create(
             {"name": "Partner", "company_id": False}
         )
-        self.child = self.env["res.partner"].create(
-            {"name": "Child", "parent_id": self.partner.id}
+        cls.child = cls.env["res.partner"].create(
+            {"name": "Child", "parent_id": cls.partner.id}
         )
-        self.payment_term_1 = self.env["account.payment.term"].create({"name": "PT 1"})
-        self.payment_term_2 = self.env["account.payment.term"].create({"name": "PT 2"})
+        if "account.payment.term" in cls.env:  # needs account installed
+            cls.payment_term_1 = cls.env["account.payment.term"].create(
+                {"name": "PT 1"}
+            )
+            cls.payment_term_2 = cls.env["account.payment.term"].create(
+                {"name": "PT 2"}
+            )
 
-    def create_company(self, name):
-        return self.env["res.company"].create({"name": name})
+    @classmethod
+    def create_company(cls, name):
+        return cls.env["res.company"].create({"name": name})
 
     def test_partner(self):
         self.partner.property_ids.invalidate_cache()
